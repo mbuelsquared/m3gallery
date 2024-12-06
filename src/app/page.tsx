be80f0738ca43fcef17ca465040c575e1083a49d
mbuelsquared <mehmet.buelbuel34@gmail.com>
2024-12-06 09:19:48 +0100

    use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,49 @@
+import { SignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 
 import { db } from "~/server/db";;
 
 
-
-
-
-export default async function HomePage() {
-
+async function  Images() {
   const images = await db.query.images.findMany({
 
     orderBy:(model, {desc}) => desc(model.id)
   });
 
 
+  return ( <div className="flex flex-wrap gap-4">
+          
+  {[...images, ...images, ...images, ...images].map((image, index) =>(
+    <div key={image.id + "-" + index} className="w-48 p-4 flex-col">
+      <img src={image.url}/>
+      <div className="text-center">{image.name}</div>
+
+
+    </div>
+  ))}
+</div>);
+
+}
+
+
+export default async function HomePage() {
+
+  
+
+
 
   return (
     <main className="">
 
-        <div className="flex flex-wrap gap-4">
-          
-          {[...images, ...images, ...images, ...images].map((image, index) =>(
-            <div key={image.id + "-" + index} className="w-48 p-4 flex-col">
-              <img src={image.url}/>
-              <div className="text-center">{image.name}</div>
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center"> Please sign in above to see more</div> 
+      </SignedOut>
 
+      <SignedIn>
+        <Images/>
+      </SignedIn>
 
-            </div>
-          ))}
-        </div>
+        
     </main>
   );
 }
